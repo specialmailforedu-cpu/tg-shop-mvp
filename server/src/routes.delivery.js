cecb51af7c82e8d96d95378d45d85e91e52d7575
specialmailforedu-cpu <specialmailforedu@gmail.com>
2026-01-17 18:33:20 +0300

    Add delivery router with token handling

--- server/src/routes.delivery.js
+++ server/src/routes.delivery.js
@@ -1,1 +1,32 @@
+import express from "express";
+import { one, run } from "./db.js";
+import { decrypt } from "./crypto.js";
+
+export const deliveryRouter = express.Router();
+
+deliveryRouter.get("/:token", (req, res) => {
+  const d = one(`SELECT * FROM deliveries WHERE secret_view_token = ?`, [req.params.token]);
+  if (!d) return res.status(404).send("Not found");
+  if (d.viewed_at) return res.status(410).send("Link already used");
+
+  const text = decrypt(d.secret_ciphertext);
+  run(`UPDATE deliveries SET viewed_at=datetime('now') WHERE id=?`, [d.id]);
+
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.send(`
+    <h2>Данные заказа</h2>
+    <pre style="white-space:pre-wrap; font-size: 16px; line-height: 1.35">${escapeHtml(text)}</pre>
+    <p><b>Внимание:</b> ссылка одноразовая и уже погашена.</p>
+  `);
+});
+
+function escapeHtml(s) {
+  return String(s).replace(/[&<>"']/g, (c) => ({
+    "&":"&amp;",
+    "<":"&lt;",
+    ">":"&gt;",
+    "\"":"&quot;",
+    "'":"&#039;"
+  }[c]));
+}
 
