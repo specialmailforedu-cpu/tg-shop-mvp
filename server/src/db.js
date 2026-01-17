e701818fa03f8b308c711074b88acc0f8e300eb4
specialmailforedu-cpu <specialmailforedu@gmail.com>
2026-01-17 18:31:45 +0300

    Update db.js

--- server/src/db.js
+++ server/src/db.js
@@ -1,1 +1,14 @@
+import Database from "better-sqlite3";
+
+export const db = new Database(process.env.DB_PATH || "./data.sqlite");
+
+export function q(sql, params = []) {
+  return db.prepare(sql).all(params);
+}
+export function one(sql, params = []) {
+  return db.prepare(sql).get(params);
+}
+export function run(sql, params = []) {
+  return db.prepare(sql).run(params);
+}
 
